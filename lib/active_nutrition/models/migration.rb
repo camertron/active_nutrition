6b84b961bb67cca60d36c5f70cb50483a24a5cb8
Cameron Dutro <camertron@gmail.com>
2013-03-03 23:54:03 -0800

    Fixing migrations

--- lib/active_nutrition/models/migration.rb
+++ lib/active_nutrition/models/migration.rb
@@ -4,6 +4,7 @@ module ActiveNutrition
   module Models
     class Migration < ActiveRecord::Base
       self.table_name = "active_nutrition_migrations"
+      attr_accessible :sequence_no
     end
   end
 end
--- lib/active_nutrition/version.rb
+++ lib/active_nutrition/version.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
 module ActiveNutrition
-  VERSION = "0.6.0"
+  VERSION = "0.6.1"
   USDA_VERSION = "24"
 end
